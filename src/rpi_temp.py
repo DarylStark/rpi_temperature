6f50aa7585af56b18acf41c0f26cfce1bf7dde32
Daryl Stark <daryl.stark@hightechcampus.com>
2023-05-09 08:44:03 +0200

    Created file structure

---
+++ src/rpi_temp.py
@@ -0,0 +1,18 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    pass
+
+
+def rpi_temp() -> None:
+    # Get the CPU temperature
+    pass
+
+    # Get the GPU temperature
+    pass
+
+
+if __name__ == '__main__':
+    # Run the main function
+    pass
